0d8583d84162699b3084eca4cea43b08a4e7d6fd
Noah Pittman <noahpittman00@gmail.com>
2023-10-01 17:24:54 -0400

    test route mapping. ready to build dashboard & user section

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -1,8 +1,21 @@
+import prismadb from "@/lib/prismadb";
+
 import Link from "next/link";
 
-export default function Page() {
+export default async function Page() {
+	const users = await prismadb.user.findMany();
+
+	console.log(users);
+
 	return (
 		<div>
+			<div>
+				{users.map((user) => (
+					<div key={user.id}>
+						{user.name}: {user.email}
+					</div>
+				))}
+			</div>
 			<Link href={"/admin"} className="btn btn-primary">
 				Return to Admin Panel
 			</Link>
--- app/admin/page.js
+++ app/admin/page.js
@@ -1,12 +1,6 @@
 import SignIn from "@/app/components/auth/SignIn";
-import { PrismaClient } from "@prisma/client";
 
 export default async function Page() {
-	const prisma = new PrismaClient();
-
-	let data = await prisma.user.findMany();
-
-	console.log(data);
 	return (
 		<>
 			<SignIn />
