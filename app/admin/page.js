6088261ebabd90dc85a022a32fc2869a2c271023
Noah Pittman <noahpittman00@gmail.com>
2023-10-01 13:43:42 -0400

    integrate prisma && planetscale

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,6 +1,13 @@
 import SignIn from "@/app/components/auth/SignIn";
+import { PrismaClient } from "@prisma/client";
 
-export default function Page() {
+export default async function Page() {
+	const prisma = new PrismaClient();
+
+	let data = await prisma.user.findMany();
+
+	console.log(data);
+
 	return (
 		<>
 			<SignIn />
