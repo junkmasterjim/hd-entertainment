c2979f118c9b4e27f6d81519c18d2fa529ca785f
Noah Pittman <noahpittman00@gmail.com>
2023-10-01 21:33:58 -0400

    update sidebar

--- app/components/DashLayout.jsx
+++ app/components/DashLayout.jsx
@@ -64,20 +64,6 @@ export default function DashLayout({ children }) {
 										))}
 									</ul>
 								</li>
-								<li className="-mx-6 mt-auto">
-									<a
-										href="#"
-										className="flex items-center gap-x-4 px-6 py-4 text-sm font-semibold leading-6 text-primary hover:bg-accent-content dark:hover:bg-primary-content"
-									>
-										<img
-											className="h-8 w-8 rounded-full bg-gray-800"
-											src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-											alt=""
-										/>
-										<span className="sr-only">Your profile</span>
-										<span aria-hidden="true">Tom Cook</span>
-									</a>
-								</li>
 							</ul>
 						</nav>
 					</div>
