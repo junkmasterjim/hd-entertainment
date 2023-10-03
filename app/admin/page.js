ee1d5469c43caa0e6eabd75dc418279e03784efe
Noah Pittman <noahpittman00@gmail.com>
2023-10-03 19:38:23 -0400

    add authentication to api

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,8 +1,6 @@
 import UsersCard from "@/app/components/admin/UsersCard";
 
 export default function Page() {
-	// const users = await prismadb.user.findMany();
-
 	return (
 		<div>
 			<UsersCard />
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,103 +1,116 @@
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
 
 import prismadb from "@/lib/prismadb";
 
-export async function GET() {
-	const users = await prismadb.user.findMany();
-	return NextResponse.json(users);
+export async function GET(req) {
+	const session = await getServerSession(req);
+	if (session) {
+		const users = await prismadb.user.findMany();
+		return NextResponse.json(users);
+	} else return NextResponse.json("401: Unauthenticated");
 }
 
 export async function POST(req) {
-	try {
-		const body = await req.json();
+	const session = await getServerSession(req);
+	if (session) {
+		try {
+			const body = await req.json();
 
-		const { name, email } = body;
+			const { name, email } = body;
 
-		if (!email) {
+			if (!email) {
+				return NextResponse.json({
+					route: "[USERS_POST]",
+					status: "ERR 400: BAD REQUEST",
+					message: "Email is required.",
+				});
+			}
+
+			const newUser = await prismadb.user.create({
+				data: {
+					name: `${body.name}` || null,
+					email: `${body.email}`,
+				},
+			});
+			return NextResponse.json(newUser);
+		} catch (err) {
 			return NextResponse.json({
-				route: "[USERS_POST]",
-				status: "ERR 400: BAD REQUEST",
-				message: "Email is required.",
+				message: "500: Internal Server Error",
+				error: err,
 			});
 		}
-
-		const newUser = await prismadb.user.create({
-			data: {
-				name: `${body.name}` || null,
-				email: `${body.email}`,
-			},
-		});
-		return NextResponse.json(newUser);
-	} catch (err) {
-		return NextResponse.json({
-			message: "500: Internal Server Error",
-			error: err,
-		});
-	}
+	} else return NextResponse.json("401: Unauthenticated");
 }
 
 export async function PATCH(req) {
-	try {
-		const body = await req.json();
-		const { name, email, id } = body;
+	const session = await getServerSession(req);
+	if (session) {
+		try {
+			const body = await req.json();
+			const { name, email, id } = body;
 
-		// console.log(name, email, id);
+			// console.log(name, email, id);
 
-		if (!name) {
+			if (!name) {
+				return NextResponse.json({
+					route: "[USERS_PATCH]",
+					status: "ERR 400: BAD REQUEST",
+					message: "Name is required.",
+				});
+			}
+			if (!email) {
+				return NextResponse.json({
+					route: "[USERS_PATCH]",
+					status: "ERR 400: BAD REQUEST",
+					message: "Email is required.",
+				});
+			}
+
+			const updateUser = await prismadb.user.update({
+				where: {
+					email: email,
+				},
+				data: {
+					name: name,
+					email: email,
+				},
+			});
+			return NextResponse.json(updateUser);
+		} catch (err) {
 			return NextResponse.json({
-				route: "[USERS_PATCH]",
-				status: "ERR 400: BAD REQUEST",
-				message: "Name is required.",
+				message: "500: Internal Server Error",
 			});
 		}
-		if (!email) {
-			return NextResponse.json({
-				route: "[USERS_PATCH]",
-				status: "ERR 400: BAD REQUEST",
-				message: "Email is required.",
-			});
-		}
-
-		const updateUser = await prismadb.user.update({
-			where: {
-				email: email,
-			},
-			data: {
-				name: name,
-				email: email,
-			},
-		});
-		return NextResponse.json(updateUser);
-	} catch (err) {
-		return NextResponse.json({
-			message: "500: Internal Server Error",
-		});
-	}
+	} else return NextResponse.json("401: Unauthenticated");
 }
 
 export async function DELETE(req) {
-	try {
-		const body = await req.json();
-		const { email } = body;
+	const session = await getServerSession(req);
+	if (session) {
+		try {
+			const body = await req.json();
+			const { email } = body;
 
-		if (!email) {
+			if (!email) {
+				return NextResponse.json({
+					route: "[USERS_PATCH]",
+					status: "ERR 400: BAD REQUEST",
+					message: "Email is required.",
+				});
+			}
+
+			const deleteUser = await prismadb.user.delete({
+				where: {
+					email: email,
+				},
+			});
+
+			return NextResponse.json(deleteUser);
+		} catch (err) {
 			return NextResponse.json({
-				route: "[USERS_PATCH]",
-				status: "ERR 400: BAD REQUEST",
-				message: "Email is required.",
+				message: "500: Internal Server Error",
 			});
 		}
-
-		const deleteUser = await prismadb.user.delete({
-			where: {
-				email: email,
-			},
-		});
-
-		return NextResponse.json(deleteUser);
-	} catch (err) {
-		return NextResponse.json({
-			message: "500: Internal Server Error",
-		});
-	}
+	} else return NextResponse.json("401: Unauthenticated");
 }
