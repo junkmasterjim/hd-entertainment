11a6b2b3b1d8e4d3e88a6712d1ae80020ef4942b
Noah Pittman <noahpittman00@gmail.com>
2023-10-03 20:02:37 -0400

    add products to pageroutes

--- app/pageRoutes.js
+++
@@ -1,6 +0,0 @@
-export default pageRoutes = [
-	{ name: "About", route: "/about" },
-	{ name: "Portfolio", route: "/portfolio" },
-	{ name: "Services", route: "/services" },
-	{ name: "Contact", route: "/about#contact" },
-];
--- pageRoutes.js
+++ pageRoutes.js
@@ -1,6 +1,7 @@
 export const pageRoutes = [
 	{ name: "About", route: "/about" },
 	{ name: "Portfolio", route: "/portfolio" },
+	{ name: "Products", route: "/products" },
 	{ name: "Services", route: "/services" },
 	{ name: "Contact", route: "/about#contact" },
 ];
