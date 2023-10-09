265a7f0dc2e321ced69043dbbf4f5489a1bf7fda
Noah Pittman <noahpittman00@gmail.com>
2023-10-09 15:40:46 -0400

    add key prop

--- app/components/PortfolioGallery.jsx
+++ app/components/PortfolioGallery.jsx
@@ -24,11 +24,11 @@ export default function PortfolioGallery() {
 		>
 			{gallery.map((content) =>
 				content.isVideo ? (
-					<div>
+					<div key={content.name}>
 						<video muted autoPlay loop src={content.imageUrl}></video>
 					</div>
 				) : (
-					<div className="relative h-96 bg-gray-500 my-8">
+					<div className="relative h-96 bg-gray-500 my-8" key={content.name}>
 						<Image
 							priority
 							src={content.imageUrl}
