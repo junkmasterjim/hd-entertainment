4cdd931cec1ba2031f7d28697d209ecb6d21edc5
Noah Pittman <noahpittman00@gmail.com>
2023-10-15 08:06:51 -0400

    update contact form

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
 	return (
-		<div className="relative isolate px-6 py-24 sm:py-32 lg:px-8">
-			<div className="mx-auto max-w-xl lg:max-w-4xl">
+		<div className="relative isolate px-6 py-24 sm:py-32 lg:px-8 ">
+			<div className="mx-auto max-w-xl lg:max-w-2xl p-4">
 				<h2 className="text-4xl font-bold tracking-tight text-primary">
 					Let’s discuss{" "}
 					<span className=" font-black underline tracking-wide">your</span>{" "}
@@ -10,7 +10,7 @@ export default function Contact() {
 				<p className="mt-2 text-lg leading-8 text-primary/75">
 					Send a message directly to my inbox.
 				</p>
-				<div className="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
+				<div className="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row ">
 					<form action="#" method="POST" className="lg:flex-auto">
 						<div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
 							<div className="sm:col-span-2">
@@ -27,7 +27,7 @@ export default function Contact() {
 										name="name"
 										id="name"
 										autoComplete="given-name"
-										className="input block w-full rounded-md border-0 px-3.5 py-2 bg-secondary/20 focus:bg-secondary/30 text-primary shadow-sm placeholder:text-primary/60 focus:ring-2 focus:ring-secondary sm:text-sm sm:leading-6"
+										className="input block w-full rounded-md border-0 px-3.5 py-2 bg-secondary/50  text-primary shadow-sm placeholder:text-primary/60 focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6"
 									/>
 								</div>
 							</div>
@@ -45,7 +45,7 @@ export default function Contact() {
 										id="message"
 										name="message"
 										rows={4}
-										className="textarea block w-full rounded-md border-0 px-3.5 py-2 text-primary bg-secondary/20 focus:bg-secondary/30 shadow-sm placeholder:text-primary/60 focus:ring-2 focus:ring-secondary sm:text-sm sm:leading-6"
+										className="textarea block w-full rounded-md border-0 px-3.5 py-2 text-primary bg-secondary/50 shadow-sm placeholder:text-primary/60 focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6"
 										defaultValue={""}
 									/>
 								</div>
@@ -61,37 +61,6 @@ export default function Contact() {
 							</button>
 						</div>
 					</form>
-					<div className="lg:mt-6 lg:w-80 lg:flex-none">
-						<img
-							className="h-12 w-auto"
-							src="https://tailwindui.com/img/logos/workcation-logo-indigo-600.svg"
-							alt=""
-						/>
-						<figure className="mt-10">
-							<blockquote className="text-lg font-semibold leading-8 text-primary">
-								<p>
-									“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
-									expedita voluptas culpa sapiente alias molestiae. Numquam
-									corrupti in laborum sed rerum et corporis.”
-								</p>
-							</blockquote>
-							<figcaption className="mt-10 flex gap-x-6">
-								<img
-									src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=96&h=96&q=80"
-									alt=""
-									className="h-12 w-12 flex-none rounded-full bg-gray-50"
-								/>
-								<div>
-									<div className="text-base font-semibold text-primary">
-										Brenna Goyette
-									</div>
-									<div className="text-sm leading-6 text-gray-600">
-										CEO of Workcation
-									</div>
-								</div>
-							</figcaption>
-						</figure>
-					</div>
 				</div>
 			</div>
 		</div>
