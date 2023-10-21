8bd94801862f0fa774d0c1094c3728a31091394d
Noah Pittman <noahpittman00@gmail.com>
2023-10-21 10:38:25 -0400

    update dependencies

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en" className="no-scrollbar scroll-smooth">
+		<html
+			lang="en"
+			className="no-scrollbar scroll-smooth"
+			suppressHydrationWarning
+		>
 			<body className={inter.className}>{children}</body>
 		</html>
 	);
