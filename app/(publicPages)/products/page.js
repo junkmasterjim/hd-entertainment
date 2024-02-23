071afe48d004f12b89c9ae723fd3124e0b21f566
Noah Pittman <noahpittman00@gmail.com>
2024-02-22 19:39:35 -0500

    Add product page and remove shop page

---
+++ app/(publicPages)/products/page.js
@@ -0,0 +1,70 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const Page = () => {
+	const [isLoading, setIsLoading] = useState(true);
+	const [products, setProducts] = useState([]);
+
+	const getProducts = async () => {
+		const response = await fetch("/api/products", {
+			headers: {
+				isFromSite: true,
+			},
+		});
+		const data = await response.json();
+
+		setProducts(data);
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		getProducts();
+	}, []);
+
+	return (
+		<>
+			<div className="pt-32 md:pt-48 max-w-screen-xl mx-auto p-4 justify-center flex flex-wrap gap-4">
+				{isLoading && <h1>Loading...</h1>}
+				{products.map((product, i) => (
+					<div className=" bg-secondary/25 text-secondary-content backdrop-blur-lg max-w-[18rem] max-h-96 w-full  overflow-hidden grid grid-rows-2 rounded-lg border border-primary/50">
+						<div>
+							<a target="_blank" href={product.url}>
+								<img
+									className="object-cover h-full w-full hover:scale-95 duration-300 ease-in-out rounded-lg hover:opacity-75 transition"
+									src={product.imageUrl}
+									alt={product.name + " image"}
+									onClick={() => {
+										console.log(products);
+									}}
+								/>
+							</a>
+						</div>
+						<div className="py-3 px-4 flex flex-col justify-between  h-full ">
+							<div className="space-y-1">
+								<h1 className="text-xl font-bold">{product.name}</h1>
+								<p className="text-sm italic">{product.description}</p>
+							</div>
+							<div className="flex justify-between items-center">
+								<p className="font-semibold">
+									{product.price === 0 ? "Free" : "$" + product.price}
+								</p>
+								<div>
+									<a
+										target="_blank"
+										href={product.url}
+										className="btn btn-sm btn-secondary"
+									>
+										{product.price === 0 ? "Download" : "Buy Now"}
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default Page;
--- pageRoutes.js
+++ pageRoutes.js
@@ -1,7 +1,7 @@
 export const pageRoutes = [
 	{ name: "About", route: "/about" },
 	{ name: "Portfolio", route: "/portfolio" },
-	// { name: "Products", route: "/products" },
+	{ name: "Products", route: "/products" },
 	// { name: "Services", route: "/services" },
 	{ name: "Contact", route: "/contact" },
 ];
